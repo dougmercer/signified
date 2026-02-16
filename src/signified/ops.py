77c810a6e1bfd80867c4c24beed6d9865638fb80
hwelch-fle <91618355+hwelch-fle@users.noreply.github.com>
2026-02-16 15:43:08 -0500

    cleanup and add special types

--- src/signified/ops.py
+++ src/signified/ops.py
@@ -12,33 +12,47 @@ all = computed(builtins.all)
 any = computed(builtins.any)
 pow = computed(builtins.pow)
 
+# Special types (operate as if they are callables, cannot subclass)
+range = computed(builtins.range)
+memoryview = computed(builtins.memoryview)
+slice = computed(builtins.slice)
+bool = computed(builtins.bool)
 
-# dead simple wrapper to run around mutator methods that send a notification after
-# mutation
-def notify_after(func: Callable[..., Any], sig: Signal[Any]) -> Callable[..., Any]:
+
+def trigger_notify(signal: Signal, func: Callable[..., Any]) -> Callable[..., Any]:
     def wrapper(*args: Any, **kwargs: Any):
         res = func(*args, **kwargs)
-        sig.notify()
+        signal.notify()
         return res
     return wrapper
 
 
 # Type Hint magic to intercept the origin type of a wrapped builtin
-class _SignalWrapper[T](Signal[T]):    
+class _SignalWrapper[T](Signal[T]):
+    __wrapped_builtins__: dict[str, type] = {}
+      
     def __init__(self, *args: Any, **kwargs: Any) -> None:
+        
+        # Extract base from hint
         base_type, *_ = get_args(self.__orig_bases__[0])
         if origin := get_origin(base_type):
             base_type = origin
-        # This currently constructs a new type on every init, but it does work
-        # definitely find a better way
-        base_type = type(f'signaled_{base_type.__name__}', (base_type, ), {})
+        
+        # Cache type wrapper or get cached wrapper
+        _wrapped_name = f'signified_{base_type.__name__}'
+        if _wrapped_name in self.__wrapped_builtins__:
+            base_type = self.__wrapped_builtins__[_wrapped_name]
+        else:
+            base_type = type(_wrapped_name, (base_type, ), {})
+            self.__wrapped_builtins__[_wrapped_name] = base_type
+        
+        # Create the signal
         super().__init__(base_type(*args, **kwargs))
         
-        if _mutators := getattr(self, '__mutators__', None):
-            for mutator in _mutators:
-                func: Callable[..., Any] = getattr(self.value, mutator)
-                setattr(self.value, mutator, notify_after(func, self))
-
+        # Apply instance wrapper on base type
+        for f in getattr(self, '__mutators__', []):
+            _trigger = trigger_notify(self, getattr(self.value, f))
+            setattr(self.value, f, _trigger)
 
 # Mutable Types
 class list[T](_SignalWrapper[builtins.list[T]]):
@@ -49,7 +63,7 @@ class dict[K, V](_SignalWrapper[builtins.dict[K, V]]):
 
 class set[T](_SignalWrapper[builtins.set[T]]):
     __mutators__ = (
-        'add', 'pop', 'clear', 'discard', 'remove', 'update'
+        'add', 'pop', 'clear', 'discard', 'remove', 'update',
         'intersection_update', '__iand__',
         'difference_update', '__isub__',
         'symmetric_difference_update', '__ixor__',
@@ -60,4 +74,3 @@ class tuple[T](_SignalWrapper[builtins.tuple[T]]): ...
 class int(_SignalWrapper[builtins.int]): ...
 class str(_SignalWrapper[builtins.str]): ...
 class frozenset(_SignalWrapper[builtins.frozenset]): ...
-
